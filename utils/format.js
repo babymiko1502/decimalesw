c9c19642d86a67648809c0e2a3af84a639dc0e49
babymiko1502 <mamibet1502@hotmail.com>
2025-08-12 12:42:50 -0500

    Delete utils directory

--- utils/format.js
+++
@@ -1,9 +0,0 @@
-function formatCOP(value) {
-  return value.toLocaleString('es-CO', {
-    style: 'currency',
-    currency: 'COP',
-    minimumFractionDigits: 2
-  });
-}
-
-module.exports = { formatCOP };
